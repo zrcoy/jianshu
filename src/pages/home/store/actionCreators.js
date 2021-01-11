d40524b6f578cd930f7a3ec6d174bf7993246595
zrcoy <zhangruichaotyler@gmail.com>
2021-01-10 20:33:36 -0500

    home page continue...

---
+++ src/pages/home/store/actionCreators.js
@@ -0,0 +1,20 @@
+import { actionCreators } from "../../../common/header/store";
+
+import axios from 'axios';
+import * as constants from './constants';
+
+const changeHomeData = (result) => ({
+  type: constants.CHANGE_HOME_DATA,
+  topicList: result.topicList,
+  articleList: result.articleList,
+  recommendList: result.recommendList
+})
+
+export const getHomeInfo = () => {
+  return (dispatch) => {
+    axios.get('api/home.json').then((res) => {
+      const result = res.data.data;
+      dispatch(changeHomeData(result));
+    })
+  }
+}
